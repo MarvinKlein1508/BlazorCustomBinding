e0e486a7a0bc445d5a2c4f4d88fd696ebf7147ec
Marvin Klein <support@marvinkleinmusic.de>
2021-03-21 16:42:52 +0100

    Projektdateien hinzufügen.

---
+++ BlazorCustomBinding/Data/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCustomBinding.Data
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ BlazorCustomBinding/Data/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCustomBinding.Data
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        // Making this an int? fixed compiler error
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+    }
+}
